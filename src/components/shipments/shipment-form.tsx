50218ed15bcbce88f852114af50128645c7aa800
Luisangel-Macbook <luisangel.esparza2022@gmail.com>
2025-08-18 19:03:09 -0500

    feat: add create shipment files needed to develop

---
+++ src/components/shipments/shipment-form.tsx
@@ -0,0 +1,12 @@
+'use client';
+
+import { useState } from 'react';
+import { useRouter } from 'next/router';
+
+export default function LoginForm() {
+  return (
+    <form className='space-y-3'>
+      
+    </form>
+  )
+}
